22bbb0986a71b5139d418d87b6104b01f8dce2ce
Marie Valentonis <mlvalentonis@protonmail.com>
2025-07-27 10:03:21 -0400

    Update HeroContent.vue

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -34,7 +34,7 @@
     border-radius: 1rem;
     box-shadow: 0 0 40px rgba(0, 0, 0, 0.5);
     max-width: 900px;
-    margin: 0 auto;
+    margin-top: -8rem;
     color: #fff;
   }
 
@@ -43,7 +43,7 @@
     font-size: 3.5rem;
     font-weight: 600;
     line-height: 4rem;
-    margin-bottom: 1rem;
+    margin: 0;
   }
 
   p {
